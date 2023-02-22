466ed96c5968c74722dfaf3be56a6f2aeab6098b
ssttt016 <ssttt016@gmail.com>
2023-02-22 19:11:19 +0900

    2023 02 22

---
+++ day02/src/day02/P55.java
@@ -0,0 +1,14 @@
+package day02;
+
+public class P55 {
+
+	public static void main(String[] args) {
+		int a = 2000000000;
+		int b = 2000000000;
+		long c = a;
+		long d = b;
+		System.out.println(c+d);
+
+	}
+
+}
