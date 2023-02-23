e9ff9dd55dc9ce987699b204524d7b7728efd4ce
ssttt016 <ssttt016@gmail.com>
2023-02-23 17:38:51 +0900

    5.38

--- day02/src/variable/P73.java
+++ day02/src/variable/P73.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class P73 {
 
 	public static void main(String[] args) {
-		String a = "1";
 	   Scanner sc = new Scanner(System.in);
 	   System.out.println("명령어를 입력 하시오(q,i,d,s)");
 	   String cmd = sc.next();
