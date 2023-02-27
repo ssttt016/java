98d0bb99dfdc7d9a129504e4129701c29a18cac6
ssttt016 <ssttt016@gmail.com>
2023-02-27 10:14:21 +0900

    0227-4

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -4,12 +4,13 @@ public class P99 {
 
 	public static void main(String[] args) {
 		
+		myfor:
 		for(int i=2;i<10;i++) {
 			System.out.printf("%d 단 \n",i);
 			for(int j=1;j<10;j++) {
 				System.out.printf("%d * %d = %d \t",i,j,(i*j));
 				if((i*j) == 21) {
-					break;
+					break myfor;
 				}
 			}
 			System.out.println("");
