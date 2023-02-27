a22afecfa0324ffa74887e946e406fd11faed099
ssttt016 <ssttt016@gmail.com>
2023-02-27 09:33:46 +0900

    tttt

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -5,6 +5,8 @@ public class P99 {
 	public static void main(String[] args) {
 		
 		int bb = 200; //깃 확인용
+		int a = 10;  //깃 확인용
+		
 		for(int i=2;i<10;i++) {
 			System.out.printf("%d 단 \n",i);
 			for(int j=1;j<10;j++) {
